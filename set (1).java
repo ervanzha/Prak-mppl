cc080479ea2926276133971cbc82b46f6273e358
ervanzha <91467311+ervanzha@users.noreply.github.com>
2022-06-28 14:50:05 +0700

    Add files via upload

---
+++ set (1).java
@@ -0,0 +1,15 @@
+package src.prakPbo_Agus.abstrakkk;
+public class set extends hitungLuas{
+    
+    public void balok(int p,int l,int t){
+        hasil = p*l*t;
+    }
+    
+    
+    
+    public void hasil(){
+        System.out.println("Luas Balok : " + (hasil));
+    }
+       
+        
+    }
